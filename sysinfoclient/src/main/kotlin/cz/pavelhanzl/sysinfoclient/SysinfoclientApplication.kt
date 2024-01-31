4785beb10af994d347cd455fce25dbd13473df5e
Pavel Hanzl <hanzlpavel77@gmail>
2024-01-31 16:53:28 +0100

    Initial commit sysinfoclient

---
+++ sysinfoclient/src/main/kotlin/cz/pavelhanzl/sysinfoclient/SysinfoclientApplication.kt
@@ -0,0 +1,11 @@
+package cz.pavelhanzl.sysinfoclient
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class SysinfoclientApplication
+
+fun main(args: Array<String>) {
+	runApplication<SysinfoclientApplication>(*args)
+}
---
+++ sysinfoclient/src/test/kotlin/cz/pavelhanzl/sysinfoclient/SysinfoclientApplicationTests.kt
@@ -0,0 +1,13 @@
+package cz.pavelhanzl.sysinfoclient
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class SysinfoclientApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
